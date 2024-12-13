a000de482ac8200576d0c2e30d3923a2250370c7
MohamedHamouch <med10hamouch@gmail.com>
2024-12-13 21:14:57 +0100

    edit car form

--- cars/cars.php
+++ cars/cars.php
@@ -49,9 +49,13 @@ $cars = mysqli_query($conn, $sql);
           echo '<p><b>Brand:</b> ' . $car['Brand'] . '</p>';
           echo '<p><b>Model:</b> ' . $car['Model'] . '</p>';
           echo '<p><b>Year:</b> ' . $car['Year'] . '</p>';
-          echo '<div class="mt-4 flex justify-between"><form action="edit_car.php" method="POST" class="inline-block">';
-          echo '<button type="submit" name="car_id" value="' . $car['Reg_number'] . '" class="bg-yellow-500 text-black text-sm py-2 px-3 rounded hover:bg-yellow-600">';
-          echo '<i class="fas fa-edit"></i> Edit</button></form>';
+          echo '<div class="mt-4 flex justify-between">';
+          echo '<button name="car_id" value="' . $car['Reg_number'] . '" 
+                  data-cbrand="' . $car['Brand'] . '" 
+                  data-cmodel="' . $car['Model'] . '" 
+                  data-cyear="' . $car['Year'] . '" 
+                  class="edit-car-btn bg-yellow-500 text-black text-sm py-2 px-3 rounded hover:bg-yellow-600">';
+          echo '<i class="fas fa-edit"></i> Edit</button>';
           echo '<form action="handel_forms/delete_car.php" method="POST" class="inline-block">';
           echo '<button type="submit" name="registration_number" value="' . $car['Reg_number'] . '" class="bg-red-500 text-white text-sm py-2 px-3 rounded hover:bg-red-600">';
           echo '<i class="fa-solid fa-trash"></i> Delete</button></form>';
@@ -68,6 +72,7 @@ $cars = mysqli_query($conn, $sql);
     </div>
   </section>
 
+  <!-- add car form -->
   <section id="add-car-popup"
     class="hidden fixed w-full h-full items-center justify-center top-0 backdrop-blur-md bg-black/70">
     <div class="items-center justify-center flex flex-col gap-4 bg-gray-200 py-8 px-12 rounded-lg">
@@ -104,6 +109,43 @@ $cars = mysqli_query($conn, $sql);
     </div>
   </section>
 
+  <!-- edit car form -->
+  <section id="edit-car-popup"
+    class="hidden fixed w-full h-full items-center justify-center top-0 backdrop-blur-md bg-black/70">
+    <div class="items-center justify-center flex flex-col gap-4 bg-gray-200 py-8 px-12 rounded-lg">
+      <button class="text-red-500 hover:text-red-700" id="close-edit-car-popup">
+        <i class="fa-solid fa-circle-xmark text-3xl"></i>
+      </button>
+
+      <form action="handel_forms/edit_car.php" method="POST" class="flex flex-col gap-6" id="edit-car-form">
+        <fieldset>
+          <label for="registration_number" class="block text-gray-800 font-semibold">Registration Number</label>
+          <input type="text" id="edit-registration_number" name="registration_number" class="w-96 p-2 border rounded-lg"
+            readonly>
+        </fieldset>
+
+        <fieldset>
+          <label for="brand" class="block text-gray-800 font-semibold">Brand</label>
+          <input type="text" id="edit-brand" name="brand" class="w-96 p-2 border rounded-lg" required>
+        </fieldset>
+
+        <fieldset>
+          <label for="model" class="block text-gray-800 font-semibold">Model</label>
+          <input type="text" id="edit-model" name="model" class="w-96 p-2 border rounded-lg" required>
+        </fieldset>
+
+        <fieldset>
+          <label for="year" class="block text-gray-800 font-semibold">Year</label>
+          <input type="number" id="edit-year" name="year" class="w-96 p-2 border rounded-lg" required>
+        </fieldset>
+
+        <button type="submit" id="submit-edit-car"
+          class="bg-blue-500 text-white font-semibold py-2 px-4 w-1/3 rounded-lg hover:bg-blue-600">Confirm</button>
+      </form>
+    </div>
+  </section>
+
+
   <footer class="bg-gray-800 text-white py-8 h-[15vh]">
     <div class="container mx-auto flex justify-between items-center">
       <a href="#" class="text-3xl font-bold">
@@ -120,7 +162,7 @@ $cars = mysqli_query($conn, $sql);
       <p>&copy; 2024 RentDrive. All rights reserved.</p>
     </div>
   </footer>
-      <script src="cars.js"></script>
+  <script src="cars.js"></script>
 </body>
 
 </html>
