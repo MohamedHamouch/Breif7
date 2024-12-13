dbad514e0b60b8b1e0bf93b43bf85db461d3c837
MohamedHamouch <med10hamouch@gmail.com>
2024-12-13 17:06:39 +0100

    edit form dynamic display js

--- clients/clients.js
+++ clients/clients.js
@@ -1,15 +1,36 @@
-const closeBtn = document.querySelector('#close-add-popup');
+//ADD
+
+const closeAddBtn = document.querySelector('#close-add-popup');
 const addClientPopup = document.querySelector('#add-client-popup');
 const addClientBtn = document.querySelector('#add-client-btn');
 
-
-closeBtn.addEventListener('click', () => {
+addClientBtn.addEventListener('click', () => {
   addClientPopup.classList.toggle('flex');
   addClientPopup.classList.toggle('hidden');
 
 });
-addClientBtn.addEventListener('click', () => {
+
+closeAddBtn.addEventListener('click', () => {
   addClientPopup.classList.toggle('flex');
   addClientPopup.classList.toggle('hidden');
+
+});
+
+//EDIT
+
+const closeEditBtn = document.querySelector('#close-edit-btn');
+const editClientPopup = document.querySelector('#edit-client-popup');
+const editClientBtns = document.querySelectorAll('.edit-client-btn');
+
+editClientBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    editClientPopup.classList.toggle('flex');
+    editClientPopup.classList.toggle('hidden');
+  })
+});
+
+closeEditBtn.addEventListener('click', () => {
+  editClientPopup.classList.toggle('flex');
+  editClientPopup.classList.toggle('hidden');
 
 });
